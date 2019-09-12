f3e4dd5c8947eedd42af95460468c7ce92d407c5
Connor Meth <connormeth27@gmail.com>
2019-09-12 19:15:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,7 @@ end
 def my_hash_creator(key, value)
   p new_hash = Hash.new("#{key} => #{value}")
 end
-my_hash_creator(:name, 'Grace Hopper')
+my_hash_creator(name:  'Grace Hopper')
 
 
 def read_from_hash(hash, key)
