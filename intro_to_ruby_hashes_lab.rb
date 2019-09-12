9c5ec301365bc8fdbb2ad3b0d2cbd58e5c0be902
Connor Meth <connormeth27@gmail.com>
2019-09-12 19:57:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  new_hash = { :"#{key}" => "#{value}"}
+  new_hash = {"#{key}" "=>" "#{value}"}
 end
 my_hash_creator(name, 'Grace Hopper')
 my_hash_creator(1, 2)
